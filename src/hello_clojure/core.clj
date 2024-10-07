80ab0df534d157eb926fbae4671172f4c81f8f6a
lyj <goooooodgood@gmail.com>
2024-10-07 21:07:58 +0900

    project.clj 메인 네임스페이스 설정, lein run 명령어 실행 시 main 함수를 찾지 못하는 문제 해결
    hello-clojure> lein run
    No :main namespace specified in project.clj.

--- src/hello_clojure/core.clj
+++ src/hello_clojure/core.clj
@@ -4,3 +4,13 @@
   "I don't do a whole lot."
   [x]
   (println x "Hello, World!"))
+
+(defn greet
+  "간단한 인사 함수"                                               ;; docstring : ex) (doc greet) 형태로 사용
+  [name]
+  (str "안녕하세요, " name "님!"))
+
+(defn -main
+  "애플리케이션 진입점"
+  [& args]
+  (println (greet "Clojure Beginner")))
