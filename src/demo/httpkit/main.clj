ad07e6fa3ae5a338a5dd3575a1145d8ebb7f6794
lyj <goooooodgood@gmail.com>
2024-10-08 20:47:47 +0900

    핸들러 함수를 defn으로 리팩토링
    - 익명 함수 대신 defn으로 명시적인 handle-request 함수 정의
    - 핸들러 함수를 분리하여 코드 가독성 및 재사용성 향상
    - 메인 함수에서 서버 실행 시 핸들러 함수를 직접 사용

--- src/demo/httpkit/main.clj
+++ src/demo/httpkit/main.clj
@@ -1,11 +1,13 @@
 (ns demo.httpkit.main
   (:require [org.httpkit.server :as http]))
 
+;; 요청을 처리하는 명시적인 핸들러 함수 정의
+(defn handle-request [req]
+  (println req)  ;; 요청 객체 출력
+  {:status 200
+   :headers {"Content-Type" "text/html"}
+   :body "Hello http-kit"})
+
+;; 메인 함수에서 http-kit 서버 실행
 (defn -main [& args]
-  (http/run-server
-    (fn [req]  ;; fn을 사용해 익명 함수로 요청 객체(req)를 받음
-      (println req)  ;; 요청 객체 출력
-      {:status 200
-       :headers {"Content-Type" "text/html"}
-       :body "Hello http-kit"})
-    {:port 8080}))
+  (http/run-server handle-request {:port 8080}))
